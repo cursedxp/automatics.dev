2126f2af0f2be36f4a6076aeaf9bba7b48b629fb
Anil Ozsoy <anilozsoy@gmail.com>
2025-03-21 20:04:58 +0300

    ✨ (layout.js): add a navbar component to the layout to improve navigation and user experience ✨ (layout.js): add a background color and scrolling behavior to the body to enhance the visual appearance and user interaction ✨ (page.js): update the content of the Home page to display a centered text with a blue background color to improve the visual design and user experience

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,13 @@
 import "./globals.css";
+import Navbar from "./components/navBar/Navbar";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="w-screen h-screen bg-amber-500 overflow-scroll">
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="max-w-screen-xl mx-auto h-full bg-blue-500">Home Page</div>
+  );
 }
