409daf6f0f7dbbe54956565794e2e1ef01e2691f
Anil Ozsoy <anilozsoy@gmail.com>
2025-03-23 19:37:02 +0300

    ✨ (Navbar.js): Improve accessibility by adding appropriate aria-labels to navigation links and buttons ♻️ (Navbar.js): Refactor div element to header element for better semantic structure

--- app/components/navBar/Navbar.js
+++ app/components/navBar/Navbar.js
@@ -2,29 +2,47 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="w-full">
-      <div className="max-w-screen-xl mx-auto flex justify-between items-center p-3">
+    <header className="w-full">
+      <nav
+        className="max-w-screen-xl mx-auto flex justify-between items-center p-3"
+        aria-label="Main navigation"
+      >
         <div className="flex items-center">
-          <p className="text-2xl font-bold">ThoughtLink</p>
+          <Link href="/" className="text-2xl font-bold">
+            <h1>ThoughtLink</h1>
+          </Link>
         </div>
-        <ul className="flex gap-12">
+        <ul className="flex gap-12" role="list">
           <li className="hover:scale-105 transition-all duration-300">
-            <Link href="/case-studies">Case studies</Link>
+            <Link href="/case-studies" aria-label="case studies">
+              Case studies
+            </Link>
           </li>
           <li className="hover:scale-105 transition-all duration-300">
-            <Link href="/about-us">About Us</Link>
+            <Link href="/about-us" aria-label="about us">
+              About Us
+            </Link>
           </li>
           <li className="hover:scale-105 transition-all duration-300">
-            <Link href="/blog">Blog</Link>
+            <Link href="/blog" aria-label="Blog">
+              Blog
+            </Link>
           </li>
           <li className="hover:scale-105 transition-all duration-300">
-            <Link href="/contact-us">Contact Us</Link>
+            <Link href="/contact-us" aria-label="contact us">
+              Contact Us
+            </Link>
           </li>
         </ul>
-        <Link href="#" className="rounded-lg py-2 px-6 bg-zinc-900 text-white">
+        <Link
+          href="#"
+          className="rounded-full py-2 px-6 bg-zinc-900 text-white hover:scale-105 hover:shadow-lg hover:cursor-pointer"
+          role="button"
+          aria-label="Book a call"
+        >
           Book A Call
         </Link>
-      </div>
-    </div>
+      </nav>
+    </header>
   );
 }
