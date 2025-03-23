62a81e8e756554d53ac63d266c037ef5981c367b
Anil Ozsoy <anilozsoy@gmail.com>
2025-03-23 19:21:19 +0300

    ✨ (HeroSection.js): add phone icon and next/image package to improve UI and UX ♻️ (HeroSection.js): refactor text animation to use framer-motion library for smoother transitions ✨ (HeroSection.js): add button animation and hover effects to improve interactivity ✨ (HeroSection.js): add user photos with animation to enhance visual appeal ♻️ (HeroSection.js): refactor positioning of absolute div to improve layout

--- app/components/heroSection/HeroSection.js
+++ app/components/heroSection/HeroSection.js
@@ -1,5 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
+import { HiPhone } from "react-icons/hi2";
+import Image from "next/image";
 export default function HeroSection() {
   return (
     <section className="">
@@ -22,11 +24,68 @@ export default function HeroSection() {
             Real-World Solutions
           </motion.span>
         </h1>
-        <p className="text-center text-gray-500 px-30 text-xl">
+        <motion.p
+          initial={{ y: 20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 0.6 }}
+          className="text-center text-gray-500 px-30 text-xl mb-10"
+        >
           At ThoughtLink, we transform your business challenges into custom
           digital solutions. From automation to tailored interfaces, we connect
           your ideas with the right technology—efficiently and thoughtfully.
-        </p>
+        </motion.p>
+        <div className="flex justify-center gap-4">
+          <motion.button
+            initial={{ y: -20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1, delay: 0.8 }}
+            className="bg-black text-white px-4 py-2 rounded-full flex items-center gap-2 hover:scale-105 hover:shadow-lg hover:cursor-pointer"
+          >
+            <HiPhone className="w-4 h-4" />
+            Book A Call
+          </motion.button>
+          <ul className="flex">
+            <motion.li
+              initial={{ x: -20, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.5, delay: 1.8 }}
+              className="relative w-10 h-10"
+            >
+              <Image
+                src="/assets/userPhotos/user-1.png"
+                alt="John Doe"
+                fill
+                className="rounded-full border-3 border-white"
+              />
+            </motion.li>
+            <motion.li
+              initial={{ x: -40, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.5, delay: 2.0 }}
+              className="relative w-10 h-10 -translate-x-2"
+            >
+              <Image
+                src="/assets/userPhotos/user-2.png"
+                alt="Jane Doe"
+                fill
+                className="rounded-full border-3 border-white"
+              />
+            </motion.li>
+            <motion.li
+              initial={{ x: -60, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.5, delay: 2.2 }}
+              className="relative w-10 h-10 -translate-x-4"
+            >
+              <Image
+                src="/assets/userPhotos/user-3.png"
+                alt="John Doe"
+                fill
+                className="rounded-full border-3 border-white"
+              />
+            </motion.li>
+          </ul>
+        </div>
         <div className="absolute top-[260px] right-[600px] w-20 h-20 transform">
           <motion.div
             className="w-10 h-10 text-black flex justify-center items-center"
