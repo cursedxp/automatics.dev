71174e5b3999885034d65f41596fb8f0210b6280
Anil Ozsoy <anilozsoy@gmail.com>
2025-03-23 22:18:03 +0300

    📝 (layout.js): add background color to body element to improve visual appearance ✨ (page.js): import and render Services component to add a new section to the page

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import Navbar from "./components/navBar/Navbar";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="w-screen h-screen overflow-scroll">
+      <body className="w-screen h-screen overflow-scroll bg-gray-100">
         <Navbar />
         {children}
       </body>
--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import HeroSection from "./components/heroSection/HeroSection";
-
+import Services from "./components/servicesSection/Services";
 export default function Home() {
   return (
-    <div className="max-w-screen-xl mx-auto h-full">
+    <div className="max-w-screen-xl mx-auto items-center h-full">
       <HeroSection />
+      <Services />
     </div>
   );
 }
