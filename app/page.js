6ed84e109c14103e0493b17fa465f5873767056e
Anil Ozsoy <anilozsoy@gmail.com>
2025-03-24 22:35:43 +0300

    ✨ (page.js): add QuoteSection component to the Home page to display a quote section on the page

--- app/page.js
+++ app/page.js
@@ -3,13 +3,14 @@ import HeroSection from "./components/home/heroSection/HeroSection";
 import Services from "./components/home/servicesSection/Services";
 import WhyUsSection from "./components/home/whyUsSection/WhyUsSection";
 import FaqSection from "./components/home/faqSection/faqSection";
-
+import QuoteSection from "./components/home/quoteSection/quoteSection";
 export default function Home() {
   return (
     <div className="max-w-screen-xl mx-auto items-center h-full">
       <HeroSection />
       <WhyUsSection />
       <Services />
+      <QuoteSection />
       <FaqSection />
     </div>
   );
