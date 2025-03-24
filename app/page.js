0a358cb3b442672f668b38a14afc5d9f7c1ad7f8
Anil Ozsoy <anilozsoy@gmail.com>
2025-03-24 12:15:59 +0300

    ♻️ (page.js): refactor import paths for HeroSection and Services components to match new file structure in the home directory

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import HeroSection from "./components/heroSection/HeroSection";
-import Services from "./components/servicesSection/Services";
+import HeroSection from "./components/home/heroSection/HeroSection";
+import Services from "./components/home/servicesSection/Services";
 export default function Home() {
   return (
     <div className="max-w-screen-xl mx-auto items-center h-full">
