241d7abc51222bf1676efaf1f16a55717ffc4711
Anil Ozsoy <anilozsoy@gmail.com>
2025-03-24 22:36:17 +0300

    ✨ (quoteSection.js): create a new component for displaying an inspirational quote on the home page 📝 (quoteSection.js): add framer-motion and useInView dependencies for animating the quote section when it comes into view 🔧 (quoteSection.js): use useRef to create a reference to the quote section element and useInView to detect when it comes into view ✨ (quoteSection.js): add motion animation to the quote section, applying a blur effect and moving it up when it comes into view 🌐 (quoteSection.js): add schema.org markup to the quote and author for better accessibility and SEO

---
+++ app/components/home/quoteSection/quoteSection.js
@@ -0,0 +1,31 @@
+"use client";
+import { motion } from "framer-motion";
+import { useInView } from "framer-motion";
+import { useRef } from "react";
+export default function QuoteSection() {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, margin: "-100px" });
+  return (
+    <motion.section
+      ref={ref}
+      className="flex-col w-full gap-10 my-24"
+      aria-label="Inspirational Quote"
+      initial={{ filter: "blur(10px)", y: 50 }}
+      animate={isInView && { filter: "blur(0px)", y: 0 }}
+      transition={{ duration: 0.8, ease: "easeOut" }}
+    >
+      <blockquote
+        className="text-center text-5xl w-2/3 mx-auto"
+        itemScope
+        itemType="https://schema.org/Quotation"
+      >
+        <p className="block font-playfair-display" itemProp="text">
+          Technology like art is a soaring exercise of the human imagination.
+        </p>
+        <cite className="text-gray-500 text-xl" itemProp="author">
+          - Daniel Bell
+        </cite>
+      </blockquote>
+    </motion.section>
+  );
+}
