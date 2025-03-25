a8b98856d49112854ac1454016813678e5bc7946
Anil Ozsoy <anilozsoy@gmail.com>
2025-03-25 21:29:10 +0300

    ✨ (page.js): add BlogSection component to the Home page to display blog posts and improve user experience

--- app/page.js
+++ app/page.js
@@ -4,6 +4,8 @@ import Services from "./components/home/servicesSection/Services";
 import WhyUsSection from "./components/home/whyUsSection/WhyUsSection";
 import FaqSection from "./components/home/faqSection/faqSection";
 import QuoteSection from "./components/home/quoteSection/quoteSection";
+import BlogSection from "./components/home/blogSection/BlogSection";
+
 export default function Home() {
   return (
     <div className="max-w-screen-xl mx-auto items-center h-full">
@@ -11,6 +13,7 @@ export default function Home() {
       <WhyUsSection />
       <Services />
       <QuoteSection />
+      <BlogSection />
       <FaqSection />
     </div>
   );
