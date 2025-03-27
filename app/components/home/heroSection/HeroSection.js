49c1e82c12f3a8078670eca1e21cbf9b9e55c935
Anil Ozsoy <anilozsoy@gmail.com>
2025-03-27 13:56:08 +0300

    📝 (HeroSection.js): update HeroSection component styling to improve responsiveness and readability

--- app/components/home/heroSection/HeroSection.js
+++ app/components/home/heroSection/HeroSection.js
@@ -5,13 +5,13 @@ import Image from "next/image";
 import Link from "next/link";
 export default function HeroSection() {
   return (
-    <section className="relative flex flex-col items-center justify-center px-10 py-40">
-      <h1 className="text-8xl text-center mb-10">
+    <section className="relative flex flex-col items-center justify-center 2xl:py-40 xl:py-40 lg:pt-40 md:pt-40 sm:pt-20 py-20">
+      <h1 className="text-center mb-10 md:text-8xl sm:text-7xl text-6xl">
         <motion.span
           initial={{ y: -50, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 1, delay: 0.2 }}
-          className="block"
+          className="text-center"
         >
           Connecting Ideas
         </motion.span>
@@ -19,7 +19,7 @@ export default function HeroSection() {
           initial={{ filter: "blur(10px)", opacity: 0 }}
           animate={{ filter: "blur(0px)", opacity: 1 }}
           transition={{ duration: 1, delay: 0.5 }}
-          className="font-playfair-display block mt-12"
+          className="font-playfair-display block mt-12 md:text-8xl text-6xl"
         >
           Real-World Solutions
         </motion.span>
@@ -96,9 +96,9 @@ export default function HeroSection() {
           </motion.div>
         </div>
       </div>
-      <div className="absolute top-[260px] right-[600px] w-20 h-20 transform">
+      <div className="absolute lg:top-[260px] md:top-[260px] sm:top-[160px] top-[145px] right-1/2 w-20 h-20 transform">
         <motion.div
-          className="w-10 h-10 text-black flex justify-center items-center"
+          className="h-10 w-fit text-black flex justify-center items-center"
           initial={{
             y: -20,
             opacity: 0,
