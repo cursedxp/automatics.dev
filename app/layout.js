d3482c6ef70ed7979129da67f165bb41fde832ce
Anil Ozsoy <anilozsoy@gmail.com>
2025-04-01 13:10:25 +0200

    feat(layout.js): add metadata for improved SEO and social sharing capabilities

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,28 @@
 import "./globals.css";
 import Navbar from "./components/home/navBar/Navbar";
+import { Metadata } from "next";
+
+export const metadata = {
+  title: {
+    default: "ThoughtLink - Your Digital Solutions Partner",
+    template: "%s | ThoughtLink",
+  },
+  description:
+    "Learn about ThoughtLink's mission to build custom digital solutions. We're more than just a tech company - we're your partner in building the future of digital solutions.",
+  keywords: [
+    "ThoughtLink",
+    "digital solutions",
+    "custom software",
+    "tech company",
+    "digital transformation",
+  ],
+  openGraph: {
+    title: "ThoughtLink - Your Digital Solutions Partner",
+    description:
+      "Learn about ThoughtLink's mission to build custom digital solutions. We're more than just a tech company - we're your partner in building the future of digital solutions.",
+    type: "website",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
