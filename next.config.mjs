46fcc4993c6ddab7c600d8c0279a84b57942c5c4
Anil Ozsoy <anilozsoy@gmail.com>
2025-04-10 11:38:35 +0200

    feat(next.config.mjs): add remotePatterns and domains for image optimization to enhance image loading capabilities in the application

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+    domains: ["images.ctfassets.net"],
+  },
+};
 
 export default nextConfig;
