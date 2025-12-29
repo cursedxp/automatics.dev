98a68c70ca88ee06010d400a6ac6a7ecab7b7d18
Mircea Preotu <mircea.preotu@gmail.com>
2025-12-29 15:59:07 +0100

    x header fix
    Description
    URLs that are missing the Content-Security-Policy response header. This header allows a website to control which resources are loaded for a page. This policy can help guard against cross-site scripting (XSS) attacks that exploit the browser's trust of the content received from the server. The SEO Spider only checks for existence of the header, and does not interrogate the policies found within the header to determine whether they are well set-up for the website. This should be performed manually.
    
    How To Fix
    Set a strict Content-Security-Policy response header across all page to help mitigate cross site scripting (XSS) and data injection attacks.

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,10 @@ const nextConfig = {
             key: "X-Content-Type-Options",
             value: "nosniff",
           },
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https:; frame-src 'self' https://www.googletagmanager.com; object-src 'none'; base-uri 'self';",
+          },
         ],
       },
     ];
