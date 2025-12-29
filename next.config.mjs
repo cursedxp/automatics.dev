7eaa980445be0e8d655504818a15a0182d475c34
Mircea Preotu <mircea.preotu@gmail.com>
2025-12-29 15:59:54 +0100

    x header fix

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,10 @@ const nextConfig = {
             key: "Content-Security-Policy",
             value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https:; frame-src 'self' https://www.googletagmanager.com; object-src 'none'; base-uri 'self';",
           },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
         ],
       },
     ];
